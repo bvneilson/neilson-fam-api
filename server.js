39b1f3fc170971767ce3f520b77c838d98669ee0
Neilson <Brennan.Neilson@henryschein.com>
2020-03-12 08:24:15 -0600

    installing express-rate-limit

--- server.js
+++ server.js
@@ -4,14 +4,21 @@ const cors = require('cors');
 const userRoutes = require('./users/user-router.js');
 const recipeRoutes = require('./recipes/recipe-router.js');
 const expressfileupload = require('express-fileupload');
+const rateLimit = require("express-rate-limit");
 
 const server = express();
+
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
 
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
 server.use(expressfileupload());
 server.use(referer);
+server.use("/api/", apiLimiter)
 server.use('/api/users', userRoutes);
 server.use('/api/recipes', recipeRoutes);
 
