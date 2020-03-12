47bbb2d7f5284dd0f86a46fe91cceeeef41a9eb9
Neilson <Brennan.Neilson@henryschein.com>
2020-03-12 08:32:12 -0600

    commenting out restriction until frontend is built and tested

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ server.use(express.json());
 server.use(helmet());
 server.use(cors());
 server.use(expressfileupload());
-server.use(referer);
+// server.use(referer);
 server.use("/api/", apiLimiter)
 server.use('/api/users', userRoutes);
 server.use('/api/recipes', recipeRoutes);
@@ -26,12 +26,12 @@ server.get('/', (req, res) => {
   res.send('Welcome to the Neilson fam api');
 })
 
-function referer(req, res, next) {
-  if(req.headers.host.includes("neilsoncookbook.com") || req.headers.host.includes("localhost:8087")) {
-    next();
-  } else {
-    res.status(403).send("You shall not pass");
-  }
-}
+// function referer(req, res, next) {
+//   if(req.headers.referer.includes("neilsoncookbook.com")) {
+//     next();
+//   } else {
+//     res.status(403).send("You shall not pass");
+//   }
+// }
 
 module.exports = server;
